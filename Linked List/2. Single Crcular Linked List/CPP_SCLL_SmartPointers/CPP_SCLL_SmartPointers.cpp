985fce86e14875b4e23e7a63e96787701f118c1e
Aman-A-Singh <amansinghr884@gmail.com>
2021-08-07 22:58:17 +0530

    Single Circular Linked List (Generic) - SmartPointers

--- Linked List/2. Single Crcular Linked List/CPP_SCLL_SmartPointers/CPP_SCLL_SmartPointers.cpp
+++ Linked List/2. Single Crcular Linked List/CPP_SCLL_SmartPointers/CPP_SCLL_SmartPointers.cpp
@@ -14,10 +14,15 @@ void test_string() {
         scll_string.append("aaa");
         scll_string.append("bbb");
         scll_string.append("ccc");
+        scll_string.append("ddd");
+        scll_string.append("eee");
+        scll_string.removeAt(5);
         scll_string.print();
-
     }
     catch (std::range_error& err) {
+        std::cout << err.what() << std::endl;
+    }
+    catch (std::out_of_range& err) {
         std::cout << err.what() << std::endl;
     }
 }
--- Linked List/2. Single Crcular Linked List/CPP_SCLL_SmartPointers/SingleCircularLinkedList.h
+++ Linked List/2. Single Crcular Linked List/CPP_SCLL_SmartPointers/SingleCircularLinkedList.h
@@ -11,6 +11,8 @@ private:
 		Node(const T&);
 	};
 	std::shared_ptr<Node> head;
+	std::shared_ptr<Node> last;
+	size_t number_of_nodes;
 
 public:
 	/*Append the node to the list*/
@@ -20,9 +22,9 @@ public:
 	/*Returns true if the list is empty*/
 	bool isEmpty();
 	/*Clears the list*/
-	//void clear();
+	void clear();
 	/*removes the node at the given position*/
-	//void removeAt(size_t);
+	void removeAt(size_t);
 };
 
 template<typename T>
@@ -50,6 +52,8 @@ void SingleCircularLinkedList<T>::append(const T& aRecord) {
 		ptr->next = node_ptr;
 	}
 	node_ptr->next = head;
+	last = node_ptr;
+	number_of_nodes++;
 	return;
 }
 
@@ -64,5 +68,55 @@ void SingleCircularLinkedList<T>::print() {
 			std::cout << ptr->data << std::endl;
 			ptr = ptr->next.get();
 		} while (ptr != head.get());
+	}
+}
+
+template <typename T>
+void SingleCircularLinkedList<T>::clear() {
+	if (isEmpty()) {
+		return;
+	}
+	else {
+		head->next.reset();
+		head.reset();
+	}
+}
+
+template<typename T>
+inline void SingleCircularLinkedList<T>::removeAt(size_t position)
+{
+	//check if the list is empty
+	if (isEmpty()) {
+		throw std::range_error("The List is Empty");
+	}
+	if (position > number_of_nodes) {
+		throw std::out_of_range("The Position is out of list");
+	}
+	if (position == 1) {
+		/* Start now is the owner of the second node.*/
+		head = head->next;
+		/*last->next will point to second node*/
+		last->next = head;
+		number_of_nodes--;
+		return;
+	}
+	else {
+		Node* ptr = head.get();
+		size_t count{ 1 };
+		while (position > (count + 1)) {
+			ptr = ptr->next.get();
+			count++;
+		}
+		/*Ptr is currently at node number (position-1)*/
+		/*next pointer of the node at (position-1) becomes the owner
+		 of the node at (position+1)*/
+		ptr->next = ptr->next->next;
+		if (position == number_of_nodes) {
+			std::shared_ptr<Node> temp_last(ptr);
+			last = temp_last;
+			temp_last = nullptr;
+		}
+		number_of_nodes--;
+		return;
 	}
 }
